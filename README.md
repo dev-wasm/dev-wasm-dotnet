d6dcdc27ef1a89bfbed51b5c7676363698f1f8fa
Brendan Burns <bburns@microsoft.com>
2022-12-09 21:22:01 +0000

    enrich code.

--- README.md
+++ README.md
@@ -24,5 +24,5 @@ Visual studio should prompt you to see if you want to relaunch the workspace in 
 
 ```sh
 dotnet build starter
-wasmtime starter/bin/Debug/net7.0/starter.wasm
+wasmtime --dir . starter/bin/Debug/net7.0/starter.wasm
 ```
--- starter/Program.cs
+++ starter/Program.cs
@@ -1,1 +1,20 @@
-Console.WriteLine("Hello dotnet world");
+class DotnetWasm {
+    public DotnetWasm() {}
+
+    public void Print() {
+        Console.WriteLine("Hello dotnet world: " + System.IO.Directory.GetCurrentDirectory());
+    }
+
+    public void Read() {
+        foreach (var line in System.IO.File.ReadLines("/test.txt", new System.Text.UTF8Encoding())) {
+            Console.WriteLine(line);
+        }
+    }
+
+    static void Main(String[] args) {
+        var dw = new DotnetWasm();
+        dw.Print();
+        dw.Read();
+    }    
+}
+
