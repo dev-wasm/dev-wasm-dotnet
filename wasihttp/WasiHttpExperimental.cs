fb5edf61e4894465707cc82fb97c956b7cd6af1e
Brendan Burns <bburns@microsoft.com>
2024-01-05 00:47:20 +0000

    Update to wasmtime 15.0.1

--- wasihttp/WasiHttpExperimental.cs
+++ wasihttp/WasiHttpExperimental.cs
@@ -22,7 +22,7 @@ public class WasiHttpExperimental
     };
 
     [MethodImpl(MethodImplOptions.InternalCall)]
-    public unsafe static extern int Req(Method method, Scheme scheme, string authority, string path, string query, string headers, string body, int *statusCode, int *handle);
+    public unsafe static extern int Req(Method method, Scheme scheme, string authority, string pathQuery, string headers, string body, int *statusCode, int *handle);
 
     [MethodImpl(MethodImplOptions.InternalCall)]
     public static extern string ReadBody(int handle);
